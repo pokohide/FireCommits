d5ce06c1d2020f931a36572527f3034afb632a97
hyde2able <hyde31415@gmail.com>
2015-11-20 17:04:21 +0900

    added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['FireCommits'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'e6f432c835a5ad0702897baf2d104e92e1e9c3d9',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
