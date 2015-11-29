d6302c90b2f6164e14fb8cd3084cd96e63d3b760
hyde2able <hyde31415@gmail.com>
2015-11-29 21:10:23 +0900

    included @ is ok

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.get('/', function(req, res) {
 		return;
 	}
 	var name = req.query.username;
-	name = name.replace(/(%40)?(\w+?)/, "$2");
+	name = name.replace(/@/, "");
 
 	getGitHubData(name, function(contributions, image, count, e) {
 		if(e){ res.render('index', {ok: false, message: e.message}); }
